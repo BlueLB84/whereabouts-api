5a2b5df8abbed1fd6b29085dab8135d2ae94aa1a
L Barbee <bluelb84@gmail.com>
2018-02-22 15:29:03 -0500

    added server start and stop functions

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const teamsRouter = require('./routers/teamsRouter');
 
 mongoose.Promise = global.Promise;
 
-
-
 app.use(morgan('common'));
 app.use(
 	cors({
@@ -22,14 +20,51 @@ app.use(
 );
 
 // Routers
-
-
-
+app.use('/api/users', usersRouter);
+app.use('/api/teams', teamsRouter);
 
 app.get('/api/*', (req, res) => {
-	res.json({ok: true});
+	res.json({ok: true}); // change to send to client root url
 });
 
 // app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.exports = {app};
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
