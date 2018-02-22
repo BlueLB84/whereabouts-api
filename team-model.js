c1ad955fe80ed742c21eb2644f59e47bb90723bd
L Barbee <bluelb84@gmail.com>
2018-02-22 17:17:07 -0500

    removed required from team motto

---
+++ team-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const teamSchema = mongoose.Schema({
+	name: {type: String, required: true, unique: true},
+	motto: {type: String, default: ''},
+	imgSrc: {type: String, default: 'https://picsum.photos/150'}	
+	bulletins: [
+		{
+			userId: {type: String},
+			text: {type: String}
+		}
+	],
+	users: Array
+});
+
+teamSchema.methods.teamApiRep = function() {
+	return {
+		teamId: this._id,
+		name: this.name,
+		motto: this.motto,
+		imgSrc: this.imgSrc,
+		bulletins: this.bulletins,
+		users: this.users
+	};
+}
+
+const Team = mongoose.model('Team', teamSchema);
+
+module.exports = { Team };
+
+
+
