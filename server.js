476dcceec87c6a14fde169fb2b803bcb86855828
L Barbee <bluelb84@gmail.com>
2018-02-22 09:48:21 -0500

    cors init setup

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(
+	cors({
+		origin: CLIENT_ORIGIN
+	})
+);
 
 app.get('/api/*', (req, res) => {
-res.json({ok: true});
+	res.json({ok: true});
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
