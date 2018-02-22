34d9c16e3849eefc2044d22637a62de486f60ad8
L Barbee <bluelb84@gmail.com>
2018-02-22 13:25:28 -0500

    set up requirements

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const cors = require('cors');
-const { CLIENT_ORIGIN, PORT } = require('./config');
+const { CLIENT_ORIGIN, PORT, DATABASE_URL } = require('./config');
 const app = express();
 
+const usersRouter = require('./routers/usersRouter');
+const teamsRouter = require('./routers/teamsRouter');
+
+mongoose.Promise = global.Promise;
+
+
+
+app.use(morgan('common'));
 app.use(
 	cors({
 		origin: CLIENT_ORIGIN
 	})
 );
 
+// Routers
+
+
+
+
 app.get('/api/*', (req, res) => {
 	res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
